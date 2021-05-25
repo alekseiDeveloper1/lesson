7648df8043a302168c50bc8a970ba502f65eb39b
Aleksei <Aleksei.developer@yandex.ru>
2021-05-25 22:55:13 +0300

    project 1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 'use strict';
 
-const numberOfFilms = +prompt('сколько фильмов вы уже смотрели?', '');
+let numberOfFilms
+
+function start() {
+  numberOfFilms = +prompt('сколько фильмов вы уже смотрели?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('сколько фильмов вы уже смотрели?', '');
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,8 +20,57 @@ const personalMovieDB = {
   privat: false
 };
 
-const setMoveFirst = prompt('Какой фильм вы смотрели?', ""),
-      thinkFirst = prompt('Ваша оценка?', "");
+for (let i = 0; i < 2; i++) {
+  const setMoveFirst = prompt('Какой фильм вы смотрели?', ""),
+        thinkFirst = prompt('Ваша оценка?', "");
+  if (setMoveFirst != null && thinkFirst != null &&
+     setMoveFirst != "" && thinkFirst != '' && setMoveFirst < 50) {
+    personalMovieDB.movies[setMoveFirst] = thinkFirst;
+  } else {
+    i--;
+  }
+}
 
-personalMovieDB.movies[setMoveFirst] = thinkFirst;
-console.log(personalMovieDB);
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const setMoveFirst = prompt('Какой фильм вы смотрели?', ""),
+          thinkFirst = prompt('Ваша оценка?', "");
+    if (setMoveFirst != null && thinkFirst != null &&
+       setMoveFirst != "" && thinkFirst != '' && setMoveFirst < 50) {
+      personalMovieDB.movies[setMoveFirst] = thinkFirst;
+    } else {
+      i--;
+    }
+  }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (numberOfFilms.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+  } else if (numberOfFilms.count > 10 && numberOfFilms.count < 30) {
+    alert('Вы классический зритель');
+  } else if (numberOfFilms > 30) {
+    alert('Вы классический зритель');
+  }
+}
+
+detectPersonalLevel();
+
+function showMyDB() {
+  if (privat == false) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+  for (let i = 0; i < 2; i++) {
+    const likeGen = prompt(`любимый жанр?${i}`, "");
+    personalMovieDB.genres[i] = likeGen;
+  }
+}
+
+writeYourGenres();
